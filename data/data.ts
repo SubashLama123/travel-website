abe4e4a0b07ab02626855acd758fe2352a1ec6fe
SubashLama123 <lamasubaah2@gmail.com>
2025-06-30 22:07:17 +0545

    ch

---
+++ data/data.ts
@@ -0,0 +1,200 @@
+export const destinationData = [
+  {
+    id: 1,
+    image: "/images/d1.jpg",
+    country: "France",
+    travelers: "150,000",
+  },
+  {
+    id: 2,
+    image: "/images/d2.jpg",
+    country: "USA",
+    travelers: "150,00",
+  },
+  {
+    id: 3,
+    image: "/images/d3.jpg",
+    country: "italy",
+    travelers: "180,000",
+
+  },
+  {
+    id: 4,
+    image: "/images/d4.jpg",
+    country: "japan",
+    travelers: "200.000",
+  },
+  {
+    id: 5,
+    image: "/images/d5.jpg",
+    country: "Australia",
+    travelers: "120,000",
+
+  },
+  {
+    id: 6,
+    image: "/images/d6.jpg",
+    country: "Canada",
+    trvelers: "175,000",
+  },
+  {
+    id: 7,
+    image: "/images/d7.jpg",
+    country: "Germany",
+    travelers: "160,00",
+  },
+  {
+    id: 8,
+    image: "/images/d8.jpg",
+    country: "Brazil",
+    travelers: "140,000",
+  },
+
+];
+export const hotelsData = [
+
+  {
+    id: 1,
+    image: "/images/h1.jpg",
+    name: "The Grand Resort and Spa",
+    location: "Westminster,London",
+    rating: 4.6,
+    reviews: "2,345",
+    price: "72",
+
+
+  },
+  {
+    id: 2,
+    image: "/images/h2.jpg",
+    name: "Barcelona city suites deluxe hotel",
+    location: "Ciutat vella, Barcelona",
+    rating: 4.7,
+    reviews: "1,912",
+    price: "85",
+
+
+  },
+  {
+    id: 3,
+    image: "/images/h3.jpg",
+    name: "Times Squre premium  Stay Hotel",
+    location: "Manhattan, New York",
+    rating: 4.9,
+    reviews: "3,420",
+    price: "95",
+
+
+  },
+  {
+    id: 4,
+    image: "/images/h4.jpg",
+    name: "Hilton Roma Luxury Hotel Palace",
+    location: "Vaticano Prati,Rome",
+    rating: 4.5,
+    reviews: "2,876",
+    price: "68",
+
+
+  },
+];
+
+export const toursData = [
+  {
+    id: 1,
+    image: "/images/t1.jpg",
+    title: "Historic wonders of Stonehenge & Oxford Tour",
+    location: "Wiltshire,England",
+    time: "12+ hours",
+    type: "Cultural & Hisorical Tours",
+    rating: 4.9,
+    reviews: "1,245",
+    price: "65",
+  },
+
+
+  {
+    id: 2,
+    image: "/images/t2.jpg",
+    title: "Barcelona Gothic Quarter walking Tour",
+    location: "Ciutat Vella,Bacelona",
+    time: "3+ hours",
+    type: "walking & Sightseeing tours",
+    rating: 4.7,
+    reviews: "876",
+    price: "50",
+  },
+  {
+    id: 3,
+    image: "/images/t3.jpg",
+    title: "Thames Luxury Boat Cruise with Dinner",
+    location: "London, United zkingdom",
+    time: "2-3 hours",
+    type: "Private & Luxury Cruises",
+    rating: 4.8,
+    reviews: "2,300",
+    price: "110",
+  },
+  {
+    id: 4,
+    image: "/images/t4.jpg",
+    title: "Edinburgh haunted History Walking Tour",
+    location: "Edinburgh,Scotland",
+    time: "2+ hours",
+    type: "Ghost & Mystery Tours",
+    rating: 4.6,
+    reviews: "1,050",
+    price: "40",
+  },
+
+]
+export const reviewData = [
+  {
+    id: 1,
+    name: "John Doe",
+    review: "The service exceeded my expectatons. The team was professional, and I couldn't be happier with the results.",
+    image: "/images/u1.jpg",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    review: "I am thoroughly impressed with the attention to detail and quality.The truly understand customer needs and .",
+    image: "/images/u2.jpg",
+  },
+  {
+    id: 3,
+    name: "Jane Smith",
+    review: "I am thoroughly impressed with the attention to detail and quality.The truly understand customer needs and .",
+    image: "/images/u3.jpg",
+  },
+  {
+    id: 4,
+    name: "Jane Smith",
+    review: "I am thoroughly impressed with the attention to detail and quality.The truly understand customer needs and .",
+    image: "/images/u4.jpg",
+  },
+  {
+    id: 5,
+    name: "Jane Smith",
+    review: "I am thoroughly impressed with the attention to detail and quality.The truly understand customer needs and .",
+    image: "/images/u5.jpg",
+  },
+  {
+    id: 6,
+    name: "sarah Johnson",
+    review: "I am thoroughly impressed with the attention to detail and quality.The truly understand customer needs and .",
+    image: "/images/u6.jpg",
+  },
+  {
+    id: 7,
+    name: "David Lee",
+    review: "I am thoroughly impressed with the attention to detail and quality.The truly understand customer needs and .",
+    image: "/images/u7.jpg",
+  },
+  {
+    id: 8,
+    name: "Sophia White",
+    review: "I am thoroughly impressed with the attention to detail and quality.The truly understand customer needs and .",
+    image: "/images/u8.jpg",
+  },
+]
