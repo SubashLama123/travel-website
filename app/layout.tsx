002f2656d5f2149afecb7dcc77ce219e920fb983
SubashLama123 <lamasubaah2@gmail.com>
2025-07-02 11:32:09 +0545

    footer created

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 // import { Poppins } from 'next/font/Local';
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Footer from "@/components/Home/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
         <ResponsiveNav />
 
         {children}
+        <Footer />
 
       </body>
     </html>
