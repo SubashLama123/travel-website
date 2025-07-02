573a640aa5c969286bd7a6a99ea9808b8d14a265
SubashLama123 <lamasubaah2@gmail.com>
2025-07-02 13:25:28 +0545

    correct hotelcard

--- components/Home/Hotel/HotelCard.tsx
+++ components/Home/Hotel/HotelCard.tsx
@@ -35,18 +35,26 @@ const HotelCard = ({ hotel }: Props) => {
       </div>
       {/* content */}
       <div>
-        <h1 className='mt-4 text-lg font-semibold text-blue-950 hover:text-black cursor-pointer transition-all duration-200'>{hotel.name}</h1>
-        <p className='text-sm text-gray-600 mt-3 font-medium mb-6'>{hotel.location}
-
+        <h1 className='mt-4 text-lg font-semibold text-blue-950 hover:text-black cursor-pointer transition-all duration-200'>
+          {hotel.name}
+        </h1>
+        <p className='text-sm text-gray-600 mt-3 font-medium mb-6'>
+          {hotel.location}
         </p>
         {/* ratings */}
         <div className='flex items-center space-x-2'>
-          <div className='px-2 py-2 bg-blue-800 rounded-md font-bold text-white text-xs'>{hotel.rating}</div>
-          <p className='text-sm text-gray-800' >Exceptional</p>
-          <p className='text-sm font-semibold text-gray-800'>{hotel.reviews}    Reviews</p>
+          <div className='px-2 py-2 bg-blue-800 rounded-md font-bold text-white text-xs'>
+            {hotel.rating}
+          </div>
+          <p className='text-sm text-gray-800'>Exceptional</p>
+          <p className='text-sm font-semibold text-gray-800'>
+            {hotel.reviews} Reviews
+          </p>
         </div>
         {/* prices */}
-        <p className='mt-3 text-gray-700 font-medium'>Starting from(" ") <span className='text-blue-600 font-bold'>US${hotel.price}</span> </p>
+        <p className='mt-3 text-gray-700 font-medium'>
+          Starting from <span className='text-blue-600 font-bold'>US${hotel.price}</span>
+        </p>
       </div>
     </div>
   );
