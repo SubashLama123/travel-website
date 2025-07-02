0d230c8aa654a45b063b6395492ee5a451d4dc56
SubashLama123 <lamasubaah2@gmail.com>
2025-07-02 13:19:30 +0545

    complete animation

--- components/Home/Hotel/Hotel.tsx
+++ components/Home/Hotel/Hotel.tsx
@@ -10,8 +10,9 @@ const Hotel = () => {
       <SectionHeading heading='Recommended Hotels' />
       <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 items-center mt-16'>
         {/* hotelcard */}
-        {hotelsData.map((data) => (
-          <div key={data.id}>
+        {hotelsData.map((data, i) => (
+          <div key={data.id} data-aos="fade-right" data-aos-anchor-placement="top-center"
+            data-aos-delay={`${i * 100}`}>
             <HotelCard hotel={data} />
           </div>
         ))}
--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -8,25 +8,33 @@ const News = () => {
       {/* Section Heading */}
       <SectionHeading heading='Exciting Travel News for You' />
       <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 items-center mt-20'>
-        <div>
-          <Newscard image="/images/n1.jpg"
+        <div data-aos="fade-left" data-aos-anchor-placement="top-center"
+        >
+          <Newscard
+            image="/images/n1.jpg"
             title="Top 10  place to visit in Austrillia"
             date="15 November 2024"
           />
         </div>
-        <div>
+        <div
+          data-aos="fade-left" data-aos-anchor-placement="top-center"
+          data-aos-delay="100">
           <Newscard image="/images/n2.jpg"
             title="Top 10  place to visit in Bangaladesh"
             date="15 November 2024"
           />
         </div>
-        <div>
+        <div
+          data-aos="fade-left" data-aos-anchor-placement="top-center"
+          data-aos-delay="200">
           <Newscard image="/images/n3.jpg"
             title="Top 10  place to visit in Pakistan"
             date="15 November 2024"
           />
         </div>
-        <div>
+        <div
+          data-aos="fade-left" data-aos-anchor-placement="top-center"
+          data-aos-delay="300">
           <Newscard image="/images/n4.jpg"
             title="Top 10  place to visit in New Zealand"
             date="15 November 2024"
